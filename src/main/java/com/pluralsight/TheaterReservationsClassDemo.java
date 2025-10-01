baf1db78c335e69961160e6d589bd47fcf183c8d
m.zepeda040803-jpg <m.zepeda040803@gmai.com>
2025-10-01 10:58:17 -0400

    fourth commit

--- src/main/java/com/pluralsight/TheaterReservationsClassDemo.java
+++ src/main/java/com/pluralsight/TheaterReservationsClassDemo.java
@@ -6,20 +6,21 @@ public class TheaterReservationsClassDemo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("How many tickets would you like you use?");
+        int ticketAmount = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("What date will you be coming (MM/dd/yyyy):");
+        String date = scanner.nextLine();
+
         System.out.println("Please enter your name: ");
         String name = scanner.nextLine();
 
-        System.out.println("What date will you be coming (MM/dd/yyy):");
-        String date = scanner.nextLine();
 
-        System.out.println("How many tickets would you like you use?");
-        int ticketAmount = scanner.nextInt();
-
+      //  3 tickets reserved for 2023-05-13 under Johnson, Geri
 
         System.out.printf("""
-                Name: %s,
-                Date: %s,
-                Number of tickets: %d,
-                """, name, date, ticketAmount);
+                %d tickets reserved for %s under %s,
+                """, ticketAmount, date, name);
     }
 }
